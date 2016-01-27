da54d3f830e2e78e93549bf6d4aae6123f8e5d3f
Sverrir <sverrir1993@gmail.com>
2016-01-27 17:58:13 +0000

    added constructor to RationalAgent

--- src/RationalAgent.java
+++ src/RationalAgent.java
@@ -2,10 +2,14 @@ import java.util.Collection;
 
 public class RationalAgent implements Agent {
 	private SearchAlgorithm search;
-
+	private World world;
+	
+	public RationalAgent(SearchAlgorithm searchAlgorithm) {
+		this.search = searchAlgorithm;
+	}
+	
 	@Override
 	public void init(Collection<String> percepts) {
-		
 	}
 
 	@Override
