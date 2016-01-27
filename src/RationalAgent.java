3f3df16320d9f5e8eab5ef7ca7628126d4dec060
Sverrir <sverrir1993@gmail.com>
2016-01-27 15:51:46 +0000

    first commit

---
+++ src/RationalAgent.java
@@ -0,0 +1,17 @@
+import java.util.Collection;
+
+public class RationalAgent implements Agent {
+
+	@Override
+	public void init(Collection<String> percepts) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String nextAction(Collection<String> percepts) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
