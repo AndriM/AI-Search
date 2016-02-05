274c4fa1d51d2c0c1f7bd2816248af6edc395b5f
kari14@ru.is <karieiriks>
2016-02-05 16:27:42 +0000

    Uniform

--- src/UniformCostSearch.java
+++ src/UniformCostSearch.java
@@ -1,7 +1,26 @@
 import java.util.Stack;
 
 public class UniformCostSearch implements SearchAlgorithm {
-
+	
+	private class UniNode extends Node {
+		private int f;		
+		public UniNode(State state, UniNode parentNode, String action) {
+			super(state, parentNode, action);
+			f = parentNode.f + 1;
+		}
+		
+		@Override
+		public int compareTo(Node node) {
+			if(!(node instanceof UniNode))
+				return super.compareTo(node);
+			UniNode ucsnode = (UniNode) node;
+			if (this.f != ucsnode.f)
+				return this.f < ucsnode.f ? 1 : 1;
+			return 0;
+			
+		}
+		
+	}
 	@Override
 	public Stack<String> getActionSequence(State initState) {
 		// TODO Auto-generated method stub
