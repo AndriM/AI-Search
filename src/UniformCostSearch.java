35b9dd081fd40576222a0b34db896d69d8004219
Bollasupa <magnusvb14@ru.is>
2016-02-06 16:13:05 +0000

    Utfaerdi Uniform-cost-search

--- src/UniformCostSearch.java
+++ src/UniformCostSearch.java
@@ -1,12 +1,21 @@
+import java.util.HashMap;
+import java.util.PriorityQueue;
 import java.util.Stack;
 
 public class UniformCostSearch implements SearchAlgorithm {
-	
+
+
+
 	private class UniNode extends Node {
 		private int f;		
 		public UniNode(State state, UniNode parentNode, String action) {
 			super(state, parentNode, action);
-			f = parentNode.f + 1;
+			if(parentNode != null) {
+				f = parentNode.f + 1;
+			}
+			else{
+				f = 0;
+			}
 		}
 		
 		@Override
@@ -15,7 +24,7 @@ public class UniformCostSearch implements SearchAlgorithm {
 				return super.compareTo(node);
 			UniNode ucsnode = (UniNode) node;
 			if (this.f != ucsnode.f)
-				return this.f < ucsnode.f ? 1 : 1;
+				return this.f < ucsnode.f ? -1 : 1;
 			return 0;
 			
 		}
@@ -23,7 +32,34 @@ public class UniformCostSearch implements SearchAlgorithm {
 	}
 	@Override
 	public Stack<String> getActionSequence(State initState) {
-		// TODO Auto-generated method stub
+		PriorityQueue<UniNode> frontier = new PriorityQueue<>();
+		HashMap<State, UniNode> explored = new HashMap<>();
+		UniNode root = new UniNode(initState, null, null);
+		frontier.add(root);
+		while(!frontier.isEmpty()){
+			UniNode curr_node = frontier.poll();
+			State curr_state = curr_node.state;
+			if(curr_node.state.isGoal()){
+				return curr_node.getSolution();
+			}
+			if(explored.containsKey(curr_state)) {
+				UniNode explored_node = explored.get(curr_state);
+				int cmp = curr_node.compareTo(explored_node);
+				if (cmp >= 0) {
+					continue;
+				}
+			}
+			explored.put(curr_state, curr_node);
+			for(String action : curr_node.state.legalActions()) {
+					UniNode childNode = new UniNode(curr_node.state.nextState(action), curr_node, action);
+					frontier.add(childNode);
+			}
+		}
+		return failure();
+	}
+
+	private Stack<String> failure() {
+		System.out.println("No solution found!");
 		return null;
 	}
 }
